214783cb3400908e0b69dd84f4796d142ae4f1d9
Shreya Singal <shreya.singal@cybergindia.local>
2023-09-01 13:15:08 +0530

    moving circles animated

--- sketches/sketch.js
+++ sketches/sketch.js
@@ -1,34 +1,48 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [ 1080, 1080 ],
+  dimensions: [ 2048, 2048 ],
   //dimensions:  'A4',
   orientation: 'landscape',
   animate:true
 };
 
 const sketch = () => {
-  let y=500;
-  let velocity=30;
+  let circles=[];
+  for(let i=0;i<100;i++){
+    circles.push(new Circle(Math.random()*2048,Math.random()*2048,Math.random()*20));
+  }
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
-
     context.fillStyle='black';
-
-    y+=velocity;
-
-    if(y>=1080 || y<=0){
-        //bounce
-        velocity*=-1
-    }
-     
-    context.beginPath();
-    context.arc(500,y,20,0,Math.PI*2);
-    context.fill();
-
- 
+    context.lineWidth=10;
+    circles.forEach(circle=>{
+      circle.draw(context);
+      circle.move();
+    })
   };
 };
 
 canvasSketch(sketch, settings);
+
+class Circle{
+  constructor(x,y,radius){
+    this.x=x;
+    this.y=y;
+    this.radius=radius;
+    this.velocityX=Math.random()*4-2;
+    this.velocityY=Math.random()*4-2;
+  }
+
+  draw(context){
+    context.beginPath();
+    context.arc(this.x,this.y,this.radius,0,Math.PI*2);
+    context.stroke();
+  }
+
+  move(){
+    this.x+=this.velocityX;
+    this.y+=this.velocityY;
+  }
+}
