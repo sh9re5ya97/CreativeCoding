59e1c95b4b31218f370399e35d01a7d411231c54
Shreya Singal <shreya.singal@cybergindia.local>
2023-09-03 23:23:42 +0530

    bubbles moving up and stopping (fizz)

--- sketches/sketch.js
+++ sketches/sketch.js
@@ -9,14 +9,17 @@ const settings = {
 
 const sketch = () => {
   let circles=[];
-  for(let i=0;i<100;i++){
-    circles.push(new Circle(Math.random()*2048,Math.random()*2048,Math.random()*20));
-  }
+  // for(let i=0;i<300;i++){
+  //   circles.push(new Circle(Math.random()*2048,Math.random()*2048,Math.random()*20+5));
+  // }
   return ({ context, width, height }) => {
-    context.fillStyle = 'white';
+    for(let i=0;i<5;i++){
+      circles.push(new Circle(Math.random()*2048,Math.random()*2048,Math.random()*30));
+    }
+    context.fillStyle = 'black';
     context.fillRect(0, 0, width, height);
-    context.fillStyle='black';
-    context.lineWidth=10;
+    //context.fillStyle='black';
+    context.lineWidth=4;
     circles.forEach(circle=>{
       circle.draw(context);
       circle.move();
@@ -27,22 +30,35 @@ const sketch = () => {
 canvasSketch(sketch, settings);
 
 class Circle{
-  constructor(x,y,radius){
+  constructor(x,y,radius,color,velocityX,velocityY){
     this.x=x;
-    this.y=y;
+    this.y=2048;
     this.radius=radius;
+    this.color=this.randomColorGen();
     this.velocityX=Math.random()*4-2;
-    this.velocityY=Math.random()*4-2;
+    this.velocityY=Math.random()*4-9;
   }
 
   draw(context){
+    context.strokeStyle=this.color;
     context.beginPath();
     context.arc(this.x,this.y,this.radius,0,Math.PI*2);
     context.stroke();
   }
-
+  randomColorGen(){
+    let blueValue = (Math.floor(Math.random() * 150)+156).toString(16).padStart(2, '0');
+        let greenValue = (Math.floor(Math.random() * 200)).toString(16).padStart(2, '0');
+        let redValue = (Math.floor(Math.random() * 250)+156).toString(16).padStart(2, '0');
+    
+      // Combine the blue value with fixed red and green values for shades of blue
+      return `#${redValue}${greenValue}${blueValue}`;
+  }
   move(){
-    this.x+=this.velocityX;
-    this.y+=this.velocityY;
+      this.x+=this.velocityX;
+    if(this.y<Math.random()*50){
+      this.velocityY=0;
+    }else{
+      this.y+=this.velocityY;
+    }
   }
 }
