34721212abb57d50039d931c352fff8f63ee57d9
Shreya Singal <shreya.singal@cybergindia.local>
2023-09-04 15:20:45 +0530

    bubbles bounce

--- sketches/sketch.js
+++ sketches/sketch.js
@@ -9,13 +9,13 @@ const settings = {
 
 const sketch = () => {
   let circles=[];
-  // for(let i=0;i<300;i++){
-  //   circles.push(new Circle(Math.random()*2048,Math.random()*2048,Math.random()*20+5));
-  // }
+  for(let i=0;i<300;i++){
+    circles.push(new Circle(Math.random()*2048,Math.random()*2048,Math.random()*20+5));
+  }
   return ({ context, width, height }) => {
-    for(let i=0;i<5;i++){
-      circles.push(new Circle(Math.random()*2048,Math.random()*2048,Math.random()*30));
-    }
+    // for(let i=0;i<5;i++){
+    //   circles.push(new Circle(Math.random()*2048,Math.random()*2048,Math.random()*30));
+    // }
     context.fillStyle = 'black';
     context.fillRect(0, 0, width, height);
     //context.fillStyle='black';
@@ -23,6 +23,7 @@ const sketch = () => {
     circles.forEach(circle=>{
       circle.draw(context);
       circle.move();
+      circle.bounce(width,height);
     })
   };
 };
@@ -32,11 +33,11 @@ canvasSketch(sketch, settings);
 class Circle{
   constructor(x,y,radius,color,velocityX,velocityY){
     this.x=x;
-    this.y=2048;
+    this.y=y;
     this.radius=radius;
     this.color=this.randomColorGen();
     this.velocityX=Math.random()*4-2;
-    this.velocityY=Math.random()*4-9;
+    this.velocityY=Math.random()*4-2;
   }
 
   draw(context){
@@ -55,10 +56,15 @@ class Circle{
   }
   move(){
       this.x+=this.velocityX;
-    if(this.y<Math.random()*50){
-      this.velocityY=0;
-    }else{
       this.y+=this.velocityY;
+  }
+
+  bounce(width,height){
+    if(this.x<=0 || this.x>=width){
+      this.velocityX*=-1;
+    }
+    if(this.y<=0 || this.y>=height){
+      this.velocityY*=-1;
     }
   }
 }
