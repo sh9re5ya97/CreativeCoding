eb36b825b8f7c4c41dcf65c8ff1eba9f44b06d46
Shreya Singal <shreya.singal@cybergindia.local>
2023-09-04 23:43:07 +0530

    circles random motion, connecting lines variable thickness

--- sketches/sketch.js
+++ sketches/sketch.js
@@ -9,17 +9,32 @@ const settings = {
 
 const sketch = () => {
   let circles=[];
-  for(let i=0;i<300;i++){
+  for(let i=0;i<150;i++){
     circles.push(new Circle(Math.random()*2048,Math.random()*2048,Math.random()*20+5));
   }
   return ({ context, width, height }) => {
-    // for(let i=0;i<5;i++){
-    //   circles.push(new Circle(Math.random()*2048,Math.random()*2048,Math.random()*30));
-    // }
-    context.fillStyle = 'black';
+    context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
-    //context.fillStyle='black';
-    context.lineWidth=4;
+    context.fillStyle='black';
+    context.strokeStyle = 'light grey';
+    for(let i=0;i<circles.length;i++){
+      const circle1=circles[i];  
+      for(let j=0;j<circles.length;j++){
+        const circle2=circles[j];
+        const distance=getDistance(circle1.x,circle2.x,circle1.y,circle2.y)
+        if(distance<250){
+            context.strokeStyle =randomColorGen() ;
+          context.lineWidth=10-distance/25;
+          context.beginPath();
+          context.moveTo(circle1.x,circle1.y);
+          context.lineTo(circle2.x,circle2.y);
+          context.stroke();
+        }
+        
+      }
+    }
+    
+    context.lineWidth=8;
     circles.forEach(circle=>{
       circle.draw(context);
       circle.move();
@@ -28,6 +43,20 @@ const sketch = () => {
   };
 };
 
+const getDistance=(x1,x2,y1,y2)=>{
+  const a = x2-x1;
+  const b = y2-y1;
+  return Math.sqrt(a*a+b*b);
+}
+
+const randomColorGen=()=>{
+  let blueValue = (Math.floor(Math.random() * 150)+156).toString(16).padStart(2, '0');
+      let greenValue = (Math.floor(Math.random() * 150)+156).toString(16).padStart(2, '0');
+      let redValue = (Math.floor(Math.random() * 150)+156).toString(16).padStart(2, '0');
+  
+    // Combine the blue value with fixed red and green values for shades of blue
+    return `#${redValue}${greenValue}${blueValue}`;
+}
 canvasSketch(sketch, settings);
 
 class Circle{
@@ -36,6 +65,7 @@ class Circle{
     this.y=y;
     this.radius=radius;
     this.color=this.randomColorGen();
+   // this.color='black';
     this.velocityX=Math.random()*4-2;
     this.velocityY=Math.random()*4-2;
   }
